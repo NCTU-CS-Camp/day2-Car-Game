6978d04e8ea5dc9905c303a4f1961758973fb120
YunchiChien <chien.yunchi@gmail.com>
2026-07-04 00:25:22 +0800

    new logic for CP3

--- score.py
+++ score.py
@@ -168,6 +168,8 @@ def update_lap_progress(car, state):
         if on_checkpoint_3:
             if "__fill_in__":    # 如果 CP2 已通過，將 CP3 設為已通過
                 state.checkpoints_passed[2] = True
+            else:                # 否則 CP2 設為尚未通過
+                state.checkpoints_passed[2] = False
         
         # Q4-3 end
 
